d6eae86231a0c68add4d289e2351bce0743cc3dd
Gayane Galstyan <galstyanvera@gmail.com>
2015-09-29 22:41:45 -0700

    added a comment about my first game

--- Stopwach_game.py
+++ Stopwach_game.py
@@ -1,7 +1,8 @@
 
 # template for "Stopwatch: The Game"
+# one of my first games
 
-import simplegui 
+import simplegui
 # define global variables
 
 a=0
@@ -16,7 +17,7 @@ result=str(x) + "/" + str(y)
 def counter():
     global count
     count += 1
-    
+
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
 def format(t):
@@ -24,34 +25,34 @@ def format(t):
     b = int(t/100)%6
     c=int(t/10)%10
     d=t%10
-    
-    clock=str(a) + ":"+ str(b) + str(c) + "." +str (d)  
+
+    clock=str(a) + ":"+ str(b) + str(c) + "." +str (d)
     return clock
-    
+
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
     timer.start()
- 
-    
-def stop():   
+
+
+def stop():
     global x,y,result
-    
+
     if (timer.is_running() == True):
         if ( d == 0 ):
             x =x + 1
         y =y +1
     result = str(x) + '/' + str(y)
     timer.stop()
-    
+
 def reset():
-    global x,y,count,result  
+    global x,y,count,result
     count = 0
     format(count)
     x = 0
     y = 0
     result = str(x) + '/' + str(y)
     timer.stop()
-    
+
 
 # define event handler for timer with 0.1 sec interval
 timer=simplegui.create_timer(100,counter)
@@ -60,7 +61,7 @@ timer=simplegui.create_timer(100,counter)
 def draw(canvas):
     canvas.draw_text(format(count),[150,150],32,"Pink")
     canvas.draw_text(result,[200,200],30,"Blue")
-    
+
 # create frame
 frame=simplegui.create_frame("stopwatch game",300,300)
 
